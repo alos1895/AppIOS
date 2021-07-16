0aeb35fa9f30d2ccccc782a291e7ccdbfb236b3f
Alonso Sanchez <alonso.sanchez@jalisco.gob.mx>
2021-07-16 10:07:16 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Alonso Sanchez on 16/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Alonso es grande")
+            .foregroundColor(.green)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Shared
+//
+//  Created by Alonso Sanchez on 16/07/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
